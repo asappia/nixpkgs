802259b28611d2f96f93a75ef4b4cd2ab159eb58
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:18:08 +0100

    python3Packages.referencing: 0.35.1 -> 0.36.1
    https://github.com/python-jsonschema/referencing/releases/tag/v0.36.1

--- pkgs/development/python-modules/referencing/default.nix
+++ pkgs/development/python-modules/referencing/default.nix
@@ -15,7 +15,7 @@
 let
   self = buildPythonPackage rec {
     pname = "referencing";
-    version = "0.35.1";
+    version = "0.36.1";
     pyproject = true;
 
     disabled = pythonOlder "3.8";
@@ -25,7 +25,7 @@ let
       repo = "referencing";
       tag = "v${version}";
       fetchSubmodules = true;
-      hash = "sha256-Ix0cpdOs7CtersdfW9daF/+BEJaV/na1WRTlYywUJV8=";
+      hash = "sha256-kMYn41tlBMPb2hMMwHg+o6sNrwp3CHRQwmBzQHsgG1I=";
     };
 
     build-system = [
@@ -54,7 +54,7 @@ let
     meta = with lib; {
       description = "Cross-specification JSON referencing";
       homepage = "https://github.com/python-jsonschema/referencing";
-      changelog = "https://github.com/python-jsonschema/referencing/releases/tag/v${version}";
+      changelog = "https://github.com/python-jsonschema/referencing/releases/tag/${src.tag}";
       license = licenses.mit;
       maintainers = with maintainers; [ fab ];
     };
