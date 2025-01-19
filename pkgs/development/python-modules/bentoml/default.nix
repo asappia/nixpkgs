8d0bcf7c439f7a66b657e84412d29caeda77a4ef
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-19 21:14:30 +0100

    python3Packages.bentoml: 1.3.7 -> 1.3.20

--- pkgs/development/python-modules/bentoml/default.nix
+++ pkgs/development/python-modules/bentoml/default.nix
@@ -74,7 +74,7 @@
 }:
 
 let
-  version = "1.3.7";
+  version = "1.3.20";
   aws = [ fs-s3fs ];
   grpc = [
     grpcio
@@ -128,7 +128,7 @@ buildPythonPackage {
     owner = "bentoml";
     repo = "BentoML";
     tag = "v${version}";
-    hash = "sha256-98SVW7f/Yn+NMfS6UIicQcoatMSm4XSJzbuJ0S/p3sg=";
+    hash = "sha256-zc/JvnEEoV21EbBHhLBWvilidXHx1pxYsBYISFg16Us=";
   };
 
   pythonRelaxDeps = [
@@ -229,7 +229,7 @@ buildPythonPackage {
   meta = with lib; {
     description = "Build Production-Grade AI Applications";
     homepage = "https://github.com/bentoml/BentoML";
-    changelog = "https://github.com/bentoml/BentoML/releases/tag/v${version}";
+    changelog = "https://github.com/bentoml/BentoML/releases/tag/${src.tag}";
     license = licenses.asl20;
     maintainers = with maintainers; [
       happysalada
