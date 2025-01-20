257bd6c44ac7b08d68d7a00647e89a477de0fb2a
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-20 02:08:16 +0100

    fixup! python3Packages.requests-futures: 1.0.1 -> 1.0.2

--- pkgs/development/python-modules/requests-futures/default.nix
+++ pkgs/development/python-modules/requests-futures/default.nix
@@ -1,34 +1,49 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchFromGitHub,
   pythonOlder,
+  setuptools,
   requests,
+  greenlet,
+  pytestCheckHook,
+  pytest-cov-stub,
+  pytest-httpbin,
+  werkzeug
 }:
 
 buildPythonPackage rec {
   pname = "requests-futures";
   version = "1.0.2";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-a361eUAzboAPrrw9q1BjYO3slHj3si3FcIWK06p0WNo=";
+  src = fetchFromGitHub {
+    owner = "ross";
+    repo = "requests-futures";
+    tag = "v${version}";
+    hash = "sha256-eUu+M9rPyvc7OaOCCnUvGliK4gicYh6hfB0Jo19Yy1g=";
   };
 
-  propagatedBuildInputs = [ requests ];
+  build-system = [ setuptools ];
 
-  # Tests are disabled because they require being online
-  doCheck = false;
+  dependencies = [ requests ];
+
+  nativeCheckInputs = [
+    greenlet
+    pytestCheckHook
+    pytest-cov-stub
+    pytest-httpbin
+    werkzeug
+  ];
 
   pythonImportsCheck = [ "requests_futures" ];
 
   meta = with lib; {
     description = "Asynchronous Python HTTP Requests for Humans using Futures";
     homepage = "https://github.com/ross/requests-futures";
-    changelog = "https://github.com/ross/requests-futures/blob/v${version}/CHANGELOG.md";
+    changelog = "https://github.com/ross/requests-futures/blob/${src.tag}/CHANGELOG.md";
     license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ applePrincess ];
   };
